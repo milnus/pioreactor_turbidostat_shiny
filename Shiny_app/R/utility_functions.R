1f10f42fc693d3f4d2e618d5c776eb9273f67c9d
Magnus G Jespersen <44769523+milnus@users.noreply.github.com>
2024-07-02 17:22:27 +0200

    Add in raw plot function

--- Shiny_app/R/utility_functions.R
+++ Shiny_app/R/utility_functions.R
@@ -42,6 +42,31 @@ plot_raw_data <- function(raw_data, filter_vector, filt_strat){
   do.call(gridExtra::grid.arrange, c(ind_plots))
 }
 
+# Plotting each raw od curve
+plot_dataframe_raw <- function(dataframe, name, filter_vector, filt_strat) {
+  if(is.null(dataframe)){return()}
+
+  # Isolate reactor name for plot title
+  plot_title <- unlist(strsplit(name, '\\.'))[2]
+
+  # Check if plot is filtered
+  if (any(grepl(plot_title, filter_vector, ignore.case = T))){
+    if (filt_strat == 'Keep'){
+      background_col <- 'green'
+    } else { background_col <- "red" }
+  } else { background_col <- "lightgrey" }
+
+  dataframe <- dataframe[!is.na(dataframe[,2]),]
+
+  p <- ggplot(dataframe, aes(x = timestamp, y = od_reading)) +
+    geom_point() +
+    ggtitle(plot_title) +
+    theme(axis.title = element_blank(),
+          panel.background = element_rect(fill = background_col, color = background_col))
+
+  return(p)
+}
+
 # Filter out pioreactors based on user feedback
 filter_reactors <- function(pioreactor_data, pios_of_interest = c(), filt_strat){
   # Check if filtering is required
